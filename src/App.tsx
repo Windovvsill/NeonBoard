458e124d40a3c83dd4cbbf470aece091531ddf8b
Steve Vezeau <svezer@live.ca>
2022-01-23 23:22:28 -0800

    init layout components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { LandingContainer } from "./layouts/Landing";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingContainer></LandingContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/Landing.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FC } from "react";
+import { Board } from "../components/drawing/board";
+import { Row } from "../components/library/container";
+
+export const LandingContainer: FC = ({children}) => {
+    return <div style={{
+        height: "100vh",
+        width: "100vw",
+        border: "1px solid red",
+    }}>
+        <Row>
+            <Board />
+        </Row>
+        {children}
+    </div>
+};
