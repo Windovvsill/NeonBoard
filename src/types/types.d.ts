85ac5240c50e96b0dab57e30bec7929eb244c0a0
Steve Vezeau <svezer@live.ca>
2022-02-03 22:14:12 -0800

    move interfaces into types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -2,3 +2,37 @@ export interface IPosition {
   x: number;
   y: number;
 }
+
+type TCoords = [IPosition, IPosition];
+type TCoordsPartial = [IPosition?, IPosition?];
+
+/**
+ * Mutable Position Filter
+ * Eg, [{x: true, y: true}, {x: false, y: false}] means
+ * that the x1 and y1 coordinates (of the top left corner)
+ * can be dragged by this anchor.
+ */
+type MPF = [{ x: boolean; y: boolean }, { x: boolean; y: boolean }];
+
+type AnchorPosition = {
+  top: number;
+  left: number;
+  bottom: number;
+  right: number;
+};
+
+interface Anchors {
+  /** Returns where the anchor should be on the plane. */
+  top: (positions: AnchorPosition) => number;
+  /** Returns where the anchor should be on the plane. */
+  left: (positions: AnchorPosition) => number;
+  /** 4 filters describing which coordinates can be changed. */
+  mutablePositionFilter: MPF;
+}
+
+interface IFourSides {
+  top: number;
+  left: number;
+  right: number;
+  bottom: number;
+}
