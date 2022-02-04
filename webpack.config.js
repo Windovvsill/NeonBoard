650a768c9d5a75f9a423bd1e11ee0152555fedb3
Steve Vezeau <svezer@live.ca>
2022-02-03 22:10:11 -0800

    try to add ts paths but it is not possible without ejecting react scripts

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+// import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+// export const resolve = {
+//   plugins: [new TsconfigPathsPlugin({})],
+// };
+
+// webpack config can not be used unless the react-scripts project it ejected
